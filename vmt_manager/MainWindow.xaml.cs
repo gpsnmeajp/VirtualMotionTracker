22e5a8b045c285f9d7818c8663b2d58d5e5a42de
gpsnmeajp <33391403+gpsnmeajp@users.noreply.github.com>
2020-06-20 20:44:49 +0900

    C#側共有メモリIF構築完了

--- vmt_manager/MainWindow.xaml.cs
+++ vmt_manager/MainWindow.xaml.cs
@@ -1,4 +1,27 @@
-﻿using System;
+﻿/*
+MIT License
+
+Copyright (c) 2020 gpsnmeajp
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+using System;
 using System.IO;
 using System.IO.MemoryMappedFiles;
 using System.Linq;
@@ -33,11 +56,11 @@ namespace vmt_manager
      * 0x10-0x13: 32bitデータサイズ(→)
      * 0x20～: 文字列領域(→)
      */
-    public partial class MainWindow : Window
+public partial class MainWindow : Window
     {
         private DispatcherTimer dispatcherTimer;
         SharedMemory sharedMemory;
-
+        int cnt = 0;
 
         public MainWindow()
         {
@@ -58,7 +81,8 @@ namespace vmt_manager
         private void GenericTimer(object sender, EventArgs e)
         {
             Console.WriteLine(">>>" + sharedMemory.ReadStringD2M());
-            sharedMemory.WriteStringM2D("Hello World");
+            sharedMemory.WriteStringM2D("Hello World"+cnt);
+            cnt++;
         }
 
 
@@ -67,8 +91,5 @@ namespace vmt_manager
             //クローズ
             Console.WriteLine("Closed");
         }
-
-
-
     }
 }
