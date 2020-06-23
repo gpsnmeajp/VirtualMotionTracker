f5a7af0a8596e42e0d0cc0303920484b5993be5e
gpsnmeajp <33391403+gpsnmeajp@users.noreply.github.com>
2020-06-23 18:02:20 +0900

    OSCPackの導入、DirectOSCの実装

--- vmt_driver_mock/sm_test_cpp/sm_test_cpp.cpp
+++ vmt_driver_mock/sm_test_cpp/sm_test_cpp.cpp
@@ -21,16 +21,74 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
-
 #include <stdio.h>
 #include <conio.h>
+#include <windows.h>
 #include "SharedMemory.h"
+#include "DirectOSC.h"
 #include "..\..\json.hpp"
+
+#include "osc/OscOutboundPacketStream.h"
+#include "osc/OscReceivedElements.h"
+#include "osc/OscPacketListener.h"
+#include "ip/UdpSocket.h"
 
 using std::string;
 using json = nlohmann::json;
+
+#include <iostream>
+#include <cstring>
+
+class OSCReceiver : public osc::OscPacketListener {
+	virtual void ProcessMessage(const osc::ReceivedMessage& m, const IpEndpointName& remoteEndpoint) override
+	{
+		try {
+			string adr = m.AddressPattern();
+			if (adr == "/test1")
+			{
+				float x;
+				float y;
+				float z;
+				osc::ReceivedMessageArgumentStream args = m.ArgumentStream();
+				args >> x >> y >> z >> osc::EndMessage;
+
+				printf("X:%lf, Y:%lf, Z:%lf\n",x,y,z);
+			}
+		}
+		catch (osc::Exception& e)
+		{
+			printf("Exp: %s\n", e.what());		
+		}
+	}
+};
+
+void sendXYZ(DirectOSC::OSC* osc,float x, float y, float z) {
+	const size_t bufsize = 1024;
+	char buf[bufsize];
+	osc::OutboundPacketStream packet(buf, bufsize);
+	packet << osc::BeginMessage("/test1")
+		<< x
+		<< y
+		<< z
+		<< osc::EndMessage;
+	osc->GetSocketTx()->Send(packet.Data(), packet.Size());
+}
+
 int main(void)
 {
+    DirectOSC::OSC *osc = DirectOSC::OSC::GetInstance();
+    
+	OSCReceiver rcv;
+    osc->Open(&rcv, 39500, 39500);
+
+    printf("Start\n");
+	sendXYZ(osc, 1.2, 3.4, 5.66);
+    getch();
+    printf("Stop\n");
+
+    osc->Close();
+    printf("End\n");
+    /*
 	SharedMemory::SharedMemory sm;
 
 	sm.open();
@@ -53,5 +111,6 @@ int main(void)
 	sm.close();
 
 	return 0;
+	*/
 }
 
