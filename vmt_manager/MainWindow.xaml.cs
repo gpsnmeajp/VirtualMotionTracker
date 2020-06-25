0ccb7644d38825894bf6d481c5d5370678fc06fc
gpsnmeajp <33391403+gpsnmeajp@users.noreply.github.com>
2020-06-25 21:13:35 +0900

    インストール機能、強調機能、buildフォルダ作成

--- vmt_manager/MainWindow.xaml.cs
+++ vmt_manager/MainWindow.xaml.cs
@@ -48,6 +48,8 @@ using Valve.VR;
 using EasyLazyLibrary;
 using System.Numerics;
 using Rug.Osc;
+using System.Reflection.Emit;
+
 namespace vmt_manager
 {
     /// <summary>
@@ -68,19 +70,33 @@ public partial class MainWindow : Window
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            Console.WriteLine("Loaded");
+            try
+            {
+                Console.WriteLine("Loaded");
 
-            dispatcherTimer = new DispatcherTimer();
-            dispatcherTimer.Interval = new TimeSpan(0, 0, 0, 0, 100);
-            dispatcherTimer.Tick += new EventHandler(GenericTimer);
-            dispatcherTimer.Start();
+                dispatcherTimer = new DispatcherTimer();
+                dispatcherTimer.Interval = new TimeSpan(0, 0, 0, 0, 100);
+                dispatcherTimer.Tick += new EventHandler(GenericTimer);
+                dispatcherTimer.Start();
 
-            rnd = new Random();
+                rnd = new Random();
 
-            util = new EasyOpenVRUtil();
-            util.StartOpenVR();
+                util = new EasyOpenVRUtil();
+                if (!util.StartOpenVR())
+                {
+                    MessageBox.Show("Please start SteamVR.");
+                    Close();
+                    return;
+                }
 
-            osc = new OSC("127.0.0.1", 39571,39570, OnBundle,OnMessage);
+                osc = new OSC("127.0.0.1", 39571, 39570, OnBundle, OnMessage);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                Close();
+                return;
+            }
         }
 
         private void OnMessage(OscMessage message)
@@ -111,14 +127,42 @@ public partial class MainWindow : Window
 
             RoomMatrixTextBox.Text =
                 String.Format("{0:0.00}, {1:0.00}, {2:0.00}, {3:0.00}\n{4:0.00}, {5:0.00}, {6:0.00}, {7:0.00}\n{8:0.00}, {9:0.00}, {10:0.00}, {11:0.00}\n", m.m0, m.m1, m.m2, m.m3, m.m4, m.m5, m.m6, m.m7, m.m8, m.m9, m.m10,m.m11);
+            if (
+                m.m0 == 0 &&
+                m.m1 == 0 &&
+                m.m2 == 0 &&
+                m.m3 == 0 &&
+                m.m4 == 0 &&
+                m.m5 == 0 &&
+                m.m6 == 0 &&
+                m.m7 == 0 &&
+                m.m8 == 0 &&
+                m.m9 == 0 &&
+                m.m10 == 0 &&
+                m.m11 == 0
+                )
+            {
+                RoomMatrixTextBox.Background = new SolidColorBrush(Color.FromRgb(255, 100, 100));
+            }
+            else {
+                RoomMatrixTextBox.Background = new SolidColorBrush(Color.FromRgb(0, 255, 0));
+            }
+
 
             var t1 = util.GetTransformBySerialNumber("VMT_0");
             if (t1 != null) {
-                CheckPositionTextBox.Text = String.Format("{0:0.00}, {1:0.00}, {2:0.00}, ", t1.position.X,t1.position.Y,t1.position.Z);
+                string roomPos = String.Format("{0:0.00}, {1:0.00}, {2:0.00}", t1.position.X, t1.position.Y, t1.position.Z);
+                CheckPositionTextBox.Text = roomPos;
+                if (roomPos == "1.00, 1.00, 1.00") {
+                    CheckPositionTextBox.Background = new SolidColorBrush(Color.FromRgb(0, 255, 0));
+                }
+                else {
+                    CheckPositionTextBox.Background = new SolidColorBrush(Color.FromRgb(255, 100, 100));
+                }
             }
             var t2 = util.GetTransformBySerialNumberRaw("VMT_0");
             if (t2 != null) {
-                CheckPositionRawTextBox.Text = String.Format("{0:0.00}, {1:0.00}, {2:0.00}, ", t2.position.X, t2.position.Y, t2.position.Z);
+                CheckPositionRawTextBox.Text = String.Format("{0:0.00}, {1:0.00}, {2:0.00}", t2.position.X, t2.position.Y, t2.position.Z);
             }
         }
 
@@ -160,11 +204,60 @@ public partial class MainWindow : Window
 
         private void CheckPositionButton(object sender, RoutedEventArgs e)
         {
-            osc.Send(new OscMessage("/TrackerPoseRoomUnity",
+            osc.Send(new OscMessage("/TrackerPoseRoomDriver",
                 0, 1, 
-                0f, 0f, 0f,
+                1f, 1f, 1f,
                 0f, 0f, 0f, 1f,
                 0f));
+        }
+
+        private void InstallButton(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string driverPath_rel = System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) + @"\..\vmt";
+                string driverPath = System.IO.Path.GetFullPath(driverPath_rel);
+                Console.WriteLine(OpenVR.RuntimePath() + @"\bin\win64");
+                Console.WriteLine(driverPath);
+
+                System.Diagnostics.Process process = new System.Diagnostics.Process();
+                process.StartInfo.WorkingDirectory = OpenVR.RuntimePath() + @"\bin\win64";
+                process.StartInfo.FileName = OpenVR.RuntimePath() + @"\bin\win64\vrpathreg.exe";
+                process.StartInfo.Arguments = "adddriver \"" + driverPath + "\"";
+                process.StartInfo.UseShellExecute = false;
+                process.Start();
+                process.WaitForExit();
+
+                MessageBox.Show("OK (ExitCode=" + process.ExitCode + ")");
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void UninstallButton(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string driverPath_rel = System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) + @"\..\vmt";
+                string driverPath = System.IO.Path.GetFullPath(driverPath_rel);
+                Console.WriteLine(OpenVR.RuntimePath() + @"\bin\win64");
+                Console.WriteLine(driverPath);
+
+                System.Diagnostics.Process process = new System.Diagnostics.Process();
+                process.StartInfo.WorkingDirectory = OpenVR.RuntimePath() + @"\bin\win64";
+                process.StartInfo.FileName = OpenVR.RuntimePath() + @"\bin\win64\vrpathreg.exe";
+                process.StartInfo.Arguments = "removedriver \"" + driverPath + "\"";
+                process.StartInfo.UseShellExecute = false;
+                process.Start();
+                process.WaitForExit();
+
+                MessageBox.Show("OK (ExitCode=" + process.ExitCode + ")");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
